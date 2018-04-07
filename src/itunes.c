23de0914033b5e5af27a06fa8074f098bf67569d
Yann <user@plusdebornes.fr>
2018-04-07 09:01:54 +0200

    Ajout de la BDD playlist, modification du code du menu

--- src/itunes.c
+++ src/itunes.c
@@ -2,9 +2,7 @@
 #include <stdlib.h>
 #include "fmod/fmod.h"
 
-int choice;
-int choice1;
-int choose;
+int choix, choix2;
 int a;
 
 FMOD_SYSTEM  *fmodsys;
@@ -14,87 +12,121 @@ FMOD_CHANNELGROUP *channelgroup;
 FMOD_CHANNEL *channel = 0;
 FMOD_BOOL etat;
 
-int MENU () {
-		while(1){
+int MENU_PRINCIPAL () {
+	
+	while(1){
 		printf("\e[1;1H\e[2J");
 		printf("---MENU---\n");
 		printf("1) ADMIN\n");
 		printf("2) USER\n");
 		printf("3) CONFIGURATION\n");
 		printf("4) PLAY SOUND\n");
+		printf("5) QUITTER\n");
 		printf("CHOIX DU MENU : ");
-		scanf("%d",&choice);
-		switch (choice) {
-			case 1:
-			printf("\e[1;1H\e[2J");
-			printf("---ADMIN CONTROL PANEL---\n");
-			printf("1) RETOUR\n");
-			printf("CHOIX : ");
-			scanf("%d",&choice1);
-			if ( choice1 == 1 )
+		scanf("%d",&choix);
+			/*if(choix != 1 || choix != 2 || choix != 3 || choix != 4 || choix != 5)
 				{
-					MENU();
-				}
-			else if (choice1 > 2)
-				printf("\e[1;1H\e[2J");
-				printf("Le choix selectionner n'existe pas\n");
-				MENU();	
-			break;
-			
-			case 2 :
-			printf("\e[1;1H\e[2J");
-			printf("---USER CONTROL PANEL---\n");
-			break;
-			
-			case 3 :
-			printf("\e[1;1H\e[2J");
-			printf("---CONFIGURATION CONTROL PANEL---\n");
-			break;
-			
-			case 4 :
-			printf("\e[1;1H\e[2J");
-			printf("---PLAY SOUND---\n");
-			FMOD_System_PlaySound(fmodsys,sound ,NULL, 0, NULL);
-			printf("1) PAUSE\n");
-			printf("2) PLAY\n");
-			printf("3) STOP\n");
-			printf("4) EXIT\n");
-			scanf("%d",&choose);
-			switch (choose) {
-				case 1:
-					FMOD_System_PlaySound(fmodsys,sound ,NULL, 1, NULL);
-				break;	
-				
-				case 2 :
-					FMOD_ChannelGroup_GetPaused(0, &etat);
-					if (etat == 1)
-						FMOD_ChannelGroup_SetPaused(0, 0); 
-					else
-						FMOD_ChannelGroup_SetPaused(0, 1);
-				break;
-				
-				case 3:
-					FMOD_System_PlaySound(fmodsys,sound ,NULL, 0, NULL);
-				break;
+					printf("Choix impossible. Choisissez une option entre 1 et 4");
+				}*/
+			//else{
+				switch (choix){
+					case 1 :
+						printf("\e[1;1H\e[2J");
+						printf("---ADMIN CONTROL PANEL---\n");
+						printf("1) RETOUR\n");
+						printf("CHOIX : ");
+						scanf("%d",&choix);
+						if(choix != 1)
+						{
+							printf("Choix impossible. Choisissez une option entre 1 et 1");
+						}
+						else
+						{
+							MENU_PRINCIPAL();
+						}
+					break;
 					
-			}
-			break;
-			
+					case 2 :
+						printf("\e[1;1H\e[2J");
+						printf("---USER CONTROL PANEL---\n");
+						printf("1) RETOUR\n");
+						printf("CHOIX : ");
+						scanf("%d",&choix);
+						if(choix != 1)
+						{
+							printf("Choix impossible. Choisissez une option entre 1 et 1");
+						}
+						else
+						{
+							MENU_PRINCIPAL();
+						}
+					break;
+					
+					case 3:
+						printf("\e[1;1H\e[2J");
+						printf("---CONFIGURATION CONTROL PANEL---\n");
+						printf("1) RETOUR\n");
+						printf("CHOIX : ");
+						scanf("%d",&choix);
+						if(choix != 1)
+						{
+							printf("Choix impossible. Choisissez une option entre 1 et 1");
+						}
+						else
+						{
+							MENU_PRINCIPAL();
+						}
+					break;
+					
+					case 4 :
+						FMOD_System_PlaySound(fmodsys, sound, NULL, 0, NULL);
+						while(choix2 != 3){
+							printf("\e[1;1H\e[2J");
+							printf("---PLAY SOUND---\n");
+							printf("1) PLAY/PAUSE\n");
+							printf("2) STOP\n");
+							printf("3) RETOUR\n");
+							printf("CHOIX DU MENU : ");
+							scanf("%d",&choix2);
+							switch (choix2) {
+								case 1:
+									FMOD_Channel_GetPaused(0, &etat);
+
+									if (etat == 1) 								// Si la chanson est en pause
+										FMOD_Channel_SetPaused(0, 0); 			// On enlève la pause
+									else 										// Sinon, elle est en cours de lecture
+										FMOD_Channel_SetPaused(0, 1); 			// On met en pause
+								break;	
+								
+								case 2 :
+									FMOD_System_PlaySound(fmodsys,sound ,NULL, 1, NULL);
+								break;	
+							}
+							MENU_PRINCIPAL();
+						}
+					break;
+					
+					case 5:
+						printf("\e[1;1H\e[2J");
+						exit(0);
+					break;
+			//}
 		}
 	}
-}	
+}
+
 
 int main()
 {		
 		FMOD_System_Create(&fmodsys);		
 		FMOD_System_Init(fmodsys, 1, FMOD_INIT_NORMAL, NULL);
-		result = FMOD_System_CreateSound(fmodsys, "/home/user/Documents/projet_algo/music/051_1.WAV", FMOD_CREATESTREAM, 0, &sound);
+		result = FMOD_System_CreateSound(fmodsys, "music/049_1.WAV", FMOD_CREATESTREAM, 0, &sound);
 		if ( result != FMOD_OK ) 
 		{
 				fprintf(stderr, "Impossible de lire le fichier audio\n");
 				exit(EXIT_FAILURE);
 		}
-		MENU();
+		MENU_PRINCIPAL();
 		scanf("%d",&a);
 		FMOD_Sound_Release(sound);
 		FMOD_System_Close(fmodsys);
